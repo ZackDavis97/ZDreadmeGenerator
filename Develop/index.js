30b35758cc5dd1fa8b8cd9eeadbdd838844fd78c
Zack Davis <jantrezac95@gmail.com>
2021-08-24 15:31:33 -0500

    questions added to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,69 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function tellUser (){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of this project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of the porject: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe installation process or skip: "
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the purpose of this project?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Which license is needed for this project?",
+            choices: [
+                "Apache",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who contributed to this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What happens if I have a problem?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Enter GitHub username here: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter email here: "
+        }
+    ]);
+}
 // TODO: Create a function to initialize app
 function init() {}
 
